1b61bf4173250dbc68bc7bd429055625a6b0a147
Michiiman <juanito.robayo@hotmail.com>
2023-11-23 18:05:24 -0500

    feat: :sparkles: Add Consulta

--- Application/Repository/GamaProductoRepository.cs
+++ Application/Repository/GamaProductoRepository.cs
@@ -23,22 +23,5 @@ public class GamaProductoRepository: GenericRepo<GamaProducto>, IGamaProducto
         return await _context.GamaProductos
         .FirstOrDefaultAsync(p =>  p.Gama == id);
     }
-    public override async Task<(int totalRegistros, IEnumerable<GamaProducto> registros)> GetAllAsync(int pageIndez, int pageSize, string search)
-    {
-        var query = _context.GamaProductos as IQueryable<GamaProducto>;
 
-        if(!string.IsNullOrEmpty(search))
-        {
-            query = query.Where(p => p.Gama.ToLower().Contains(search));
-        }
-
-        query = query.OrderBy(p => p.Gama);
-        var totalRegistros = await query.CountAsync();
-        var registros = await query 
-            .Skip((pageIndez - 1) * pageSize)
-            .Take(pageSize)
-            .ToListAsync();
-
-        return (totalRegistros, registros);
-    }
 }
